01bc66625128f3279d87f62b0fd68d317146ea68
thiscode8888 <thiscode8888@gmail.com>
2024-11-02 21:25:25 +0100

    feat: now using webdriver-manager to avoid the need of chromedriver.exe

--- tests/config.py
+++ tests/config.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
 mobile_emulation = {"deviceName": "Nexus 5"}
@@ -10,6 +11,6 @@ def config_driver():
     options.add_argument("start-maximized")
     options.add_argument('log-level=3')
     options.add_experimental_option("mobileEmulation", mobile_emulation)
-    driver = webdriver.Chrome(service=Service('./chromedriver'), options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.implicitly_wait(2)
     return driver
